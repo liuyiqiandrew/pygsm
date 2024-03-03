7708fe33a5e29dfd409990820288d4cb86e133fc
Andrew Liu <yl9946@della8.princeton.edu>
2024-03-03 16:32:22 -0500

    add license and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pygsm',
+    version='0.1.0',
+    description='A gaussian version of PySM'
+    license='MIT',
+    packages=['pygsm'],
+    author='Yiqi Liu',
+    author_email='andrew.liu@princeton.edu',
+    url='http://github.com/liuyiqiandrew/pygsm'
+)
